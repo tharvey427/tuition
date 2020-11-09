7fee575955a1db2a07b53ca70fa76a70dee758de
Tammy <34216145+tharvey427@users.noreply.github.com>
2020-11-09 10:05:24 -0500

    updating ui

--- mmtr/src/app/services/employee.service.ts
+++ mmtr/src/app/services/employee.service.ts
@@ -4,17 +4,18 @@ import { Employee } from '../models/employee';
 import { EndpointsService } from '../constants/endpoints.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import {SessionStorage, SessionStorageService} from 'angular-web-storage';
+import { SessionStorage, SessionStorageService } from 'angular-web-storage';
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+  private headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
   private employee: Employee;
   private readonly baseUrl = 'http://localhost:8080';
   // appURL = this.endpoints.login();
+
 
   constructor(private http: HttpClient, private endpoints: EndpointsService,
               public session: SessionStorageService, private cookie: CookieService) { }
@@ -22,6 +23,11 @@ export class EmployeeService {
   getAllEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>(this.endpoints.getAllEmployees);
   }
+
+  // getAllEmployees(): Observable<Employee[]> {
+  //   return this.http.get('./assets/employees.json');
+  // }
+
 
   // getEmployee(employeeId: number): Observable<Employee> {
   //   return this.http.get<Employee>(this.endpoints.getEmployee);
@@ -35,14 +41,14 @@ export class EmployeeService {
     if (username && password) {
       const body = `username=${username}&password=${password}`;
       return this.http.post(this.endpoints.login, body,
-        {headers: this.headers, withCredentials: true}).pipe(
-        map(resp => {
-          const loggedUser: Employee = resp as Employee;
-          sessionStorage.setItem('employee', loggedUser.first);
-          // this.session.set('Employee', JSON.stringify(this.employee));
-          return loggedUser;
-        })
-      );
+        { headers: this.headers, withCredentials: true }).pipe(
+          map(resp => {
+            const loggedUser: Employee = resp as Employee;
+            sessionStorage.setItem('employee', loggedUser.first);
+            // this.session.set('Employee', JSON.stringify(this.employee));
+            return loggedUser;
+          })
+        );
     }
   }
 
